97c585fd79321c4f7016632b48788f82de298023
Max Chmelicek <max.chmelicek@gmail.com>
2024-08-18 12:05:10 +0200

    update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3040;
+const port = 3040;
 require("dotenv").config();
 
 //================ Enabling express to use body of the request ================================
